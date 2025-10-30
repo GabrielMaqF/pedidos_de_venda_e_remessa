16346c189f99b791f4499e8c0d44966a0c2ad6e9
GabrielMaqF <gabriel@maqfiltros.com.br>
2025-10-30 16:17:28 -0300

    Config update e Log Empresa

--- src/main/java/com/automatizacoes_java/pedidos_de_venda_e_remessa/config/Testador.java
+++ src/main/java/com/automatizacoes_java/pedidos_de_venda_e_remessa/config/Testador.java
@@ -1,5 +1,6 @@
 package com.automatizacoes_java.pedidos_de_venda_e_remessa.config;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
@@ -9,23 +10,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 
+import com.automatizacoes_java.pedidos_de_venda_e_remessa.domain.entidade.EmpresaEntity;
 import com.automatizacoes_java.pedidos_de_venda_e_remessa.domain.service.ClienteFornecedorService;
+import com.automatizacoes_java.pedidos_de_venda_e_remessa.domain.service.EmpresaService;
 
 
 @Configuration
 @Profile("test")
 public class Testador implements CommandLineRunner {
+	
+	private final EmpresaService eService;
     private static final Logger logger = LoggerFactory.getLogger(Testador.class);
     private final ClienteFornecedorService cfService;
 
-    public Testador(ClienteFornecedorService cfService) {
-        this.cfService = cfService;
+    public Testador(ClienteFornecedorService cfService, EmpresaService eService) {
+		this.cfService = cfService;
+		this.eService = eService;
+        
     }
 
     @Override
     public void run(String... args) {
         logger.info("--- INICIADO ---");
-
+        	
+        List<EmpresaEntity> le = eService.findAll();
+        
+        
+        if(le.isEmpty()) {
+        	logger.info("Sem Empresa");
+        	return;
+        }
+        
+        logger.info("Total Empresa: ", le.size());
+        for(EmpresaEntity e : le) {
+        	logger.info(e.toString());
+        }
+        
+        
+        
         CompletableFuture<ResponseEntity<?>> fut = cfService.getAllOmieUpdateBDA();
 
         fut.whenComplete((res, ex) -> {
